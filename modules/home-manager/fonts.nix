86cf3ba19c019a814586f2f35b6c1b17dde26d01
Itsuki Abe <itsuki.abe@itsuki.me>
2026-01-07 12:49:32 +0900

    Replace existing fonts with Japanese-compatible versions Main features: - Modified to use Noto Fonts instead of Monospace by default - Specifies Hackgen for Monospace fonts
    This ensures proper display of Japanese characters.

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -1,11 +1,14 @@
 { ... }:
 {
-  fonts.fontconfig = {
-    enable = true;
-    defaultFonts = {
-      serif = [ "Noto Serif" ];
-      sansSerif = [ "Noto Sans" ];
-      monospace = [ "Caskaydia Mono Nerd Font" ];
+  fonts = {
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [ "Noto Serif CJK JP" "Noto Color Emoji" ];
+        sansSerif = [ "Noto Sans CJK JP" "Noto Color Emoji" ];
+        monospace = [ "HackGen35 NF" "Noto Color Emoji" ];
+        emoji = [ "Noto Color Emoji"];
+      };
     };
   };
 }
